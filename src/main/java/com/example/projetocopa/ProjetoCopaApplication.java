f0012242d945b31a1a77dddb22bd1a57008416f6
KaiqueSM <kaiquesmendonca@hotmail.com>
2022-11-21 17:46:15 -0300

    Criando perfis no properties e classe main

--- src/main/java/com/example/projetocopa/ProjetoCopaApplication.java
+++ src/main/java/com/example/projetocopa/ProjetoCopaApplication.java
@@ -2,12 +2,14 @@ package com.example.projetocopa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjetoCopaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetoCopaApplication.class, args);
+        System.out.println(new BCryptPasswordEncoder().encode("1234"));
     }
 
 }
